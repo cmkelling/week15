fd9c941a38e9ee362735edf68f450e14b4ac8042
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-07 21:41:07 -0500

    commit

---
+++ houses/src/components/House.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NewRoomForm } from './NewRoomForm';
+
+export const House = (props) => {
+    const { house, updateHouse } = props;
+
+    const deleteRoom = (roomID) => {
+        const updatedHouse = {
+            ...house,
+            rooms: house.rooms.filter((x) => x._id !== roomID)
+        };
+        updateHouse(updatedHouse);
+    }
+
+    const addNewRoom = (room) => {
+        return updateHouse({ ...house, rooms: [...house.rooms, room] })
+    };
+
+    const rooms = () => {
+        <ul>
+            {house.rooms.map((room, index) => {
+                <li key={ index }>
+                    <label>{`${room.name} Area: ${room.area}`}</label>
+                </li>,
+                <button onClick={(e) => deleteRoom(room._id)}>Delete</button>
+            })}
+        </ul>
+    };
+
+    return (
+        <div>
+            <h1>{house.name}</h1>
+            {
+                rooms({ rooms, houseId: house._id, deleteRoom})
+            }
+            <NewRoomForm addNewRoom={addNewRoom}/>
+        </div>
+    );
+}
--- houses/src/components/HousesList.js
+++ houses/src/components/HousesList.js
@@ -14,5 +14,24 @@ export class HousesList extends React.Component {
     fetchHouses = async () => {
         const houses = await housesApi.get();
         this.setState({ houses });
+    };
+
+    updateHouse = async (updatedHouse)=> {
+        await houses.put(updatedHouse);
+        this.fetchHouses();
+    };
+
+    render() {
+        return(
+            <div className='house-list'>
+                {this.state.houses.map((house) => (
+                    <House
+                        house={house}
+                        key={house._id}
+                        updateHouse={this.updateHouse}
+                    />
+                ))}
+            </div>
+        )
     }
 }
